5ff277df52d9cbd12be5fb887e84517122346f6e
Michael Jordan <mikesjordan@gmail.com>
2017-04-11 07:41:32 -0600

    caps free about mikej

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>About Michael Jordan</title>
+		<meta name="description" content="Michael Jordan Bio">
+		<meta name="author" content="Michael Jordan			">
+	</head>
+	<body>
+		<p align="center">This is a page about myself. <b>Michael Jordan</b> <em>NOT related to the basketball player.</em></p>
+		<p>Born and raised in the Greater Metro Detroit area of Michigan. After being a self-employed commercial cleaning contracter for 10 years since the age of 18 I decided it was
+			time for a change. Packed up everything and moved to ABQ New Mexico to be closer to family and go to college. After learning he did not qualify for student loans
+			because of having just moved to New Mexico. He enrolled in a 10 week Deep Dive Full Stack coding bootcamp.</p>
+		<h1>Fun Sized Mike</h1>
+		<img src="images/mj1.jpg" alt="fun sized mj" align="centered">
+		<h2>Do you even lift Mike</h2>
+		<img src="images/mj2.jpg" alt="Do you even lift image" align="centered">
+		<p>The End</p>
+	</body>
+</html>
