eb95cb4bf953223f437a0aef9c699db03163d481
Michael Jordan <mikesjordan@gmail.com>
2017-04-21 14:38:12 -0600

    changed up page for my golden circle

--- index.php
+++ index.php
@@ -4,19 +4,16 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>About Michael Jordan</title>
-		<meta name="description" content="Michael Jordan Bio">
+		<title>Elevator Pitch</title>
+		<meta name="description" content="Michael Jordan Elevator">
 		<meta name="author" content="Michael Jordan			">
 	</head>
 	<body>
-		<p align="center">This is a page about myself. <b>Michael Jordan</b> <em>NOT related to the basketball player.</em></p>
-		<p>Born and raised in the Greater Metro Detroit area of Michigan. After being a self-employed commercial cleaning contracter for 10 years since the age of 18 I decided it was
-			time for a change. Packed up everything and moved to ABQ New Mexico to be closer to family and go to college. After learning he did not qualify for student loans
-			because of having just moved to New Mexico. He enrolled in a 10 week Deep Dive Full Stack coding bootcamp.</p>
-		<h1>Fun Sized Mike</h1>
-		<img src="images/mj1.jpg" alt="fun sized mj" align="centered">
-		<h2>Do you even lift Mike</h2>
-		<img src="images/mj2.jpg" alt="Do you even lift image" align="centered">
-		<p>The End</p>
+		<h1>Hey I'm <b>Michael Jordan</b>.</h1>
+		<p>I build websites and apps to bring down wages in the work place. Automation is the future.</p>
+		<p>Mcdonalds and Wendys have already implemented automated ordering templates. So why haven't you?</p>
+		<p>My mobile platform, that I will build to suit your POS and your customer base, is inexpensive and very user
+		friendly.</p>
+		<img src="whyhowwhat.svg" alt="why how what">
 	</body>
 </html>
